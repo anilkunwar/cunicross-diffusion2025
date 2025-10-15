4258bc061cb36d2f35d4bd26334d1e8a92174da5
Anil Kunwar <anilkunwar318@yahoo.com>
2025-10-15 18:08:28 +0200

    Update self-diffusionNi-ly-90.py

--- pinn/diffusionApp/multivariate_ni_bottom_sidebc_modeling/cloud_computing/self-diffusionNi-ly-90.py
+++ pinn/diffusionApp/multivariate_ni_bottom_sidebc_modeling/cloud_computing/self-diffusionNi-ly-90.py
@@ -32,7 +32,8 @@ logger = logging.getLogger(__name__)
 # Fixed parameters
 C_CU_TOP = 0.0    # Top boundary (y=Ly): Cu-poor
 C_CU_BOTTOM = 0.0     # Bottom (y=0): Cu-poor
-C_NI_TOP = 1.3e-4    #  Top (y=Ly): Ni-rich
+#C_NI_TOP = 1.3e-4    #  Top (y=Ly): Ni-rich
+C_NI_TOP = 0.0    #  Top (y=Ly): Ni-poor
 C_NI_BOTTOM = 1.3e-4      # Bottom (y=0): Ni-rich
 Ly = 90.0             # Domain height (μm)
 Lx = 60.0             # Domain width (μm)
