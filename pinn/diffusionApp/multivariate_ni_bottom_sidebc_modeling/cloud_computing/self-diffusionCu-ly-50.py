f9525559325dc4e0de88138c1e4ae0adbdbea087
Anil Kunwar <anilkunwar318@yahoo.com>
2025-10-19 18:28:41 +0200

    Update self-diffusionCu-ly-50.py

--- pinn/diffusionApp/multivariate_ni_bottom_sidebc_modeling/cloud_computing/self-diffusionCu-ly-50.py
+++ pinn/diffusionApp/multivariate_ni_bottom_sidebc_modeling/cloud_computing/self-diffusionCu-ly-50.py
@@ -30,7 +30,7 @@ logging.basicConfig(level=logging.INFO, filename=os.path.join(OUTPUT_DIR, 'train
 logger = logging.getLogger(__name__)
 
 # Fixed parameters
-C_CU_TOP = 2.85e-3    # Top boundary (y=Ly): Cu-rich
+C_CU_TOP = 1.59e-3 #2.85e-3    # Top boundary (y=Ly): Cu-rich
 C_CU_BOTTOM = 0.0     # Bottom (y=0): Cu-poor
 #C_CU_BOTTOM = 2.85e-3     # Bottom (y=0): Cu-rich
 C_NI_TOP = 0.0     #  Top (y=Ly): Ni-poor
