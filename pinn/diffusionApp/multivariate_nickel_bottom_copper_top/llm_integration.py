8a43e95ef8fc16bf51638f777a1391a2d5cda211
Anil Kunwar <anilkunwar318@yahoo.com>
2025-11-03 00:09:26 +0100

    Update llm_integration.py

--- pinn/diffusionApp/multivariate_nickel_bottom_copper_top/llm_integration.py
+++ pinn/diffusionApp/multivariate_nickel_bottom_copper_top/llm_integration.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain_core.messages import HumanMessage
 
 st.set_page_config(page_title="Lightweight LLM Integration Demo")
 
