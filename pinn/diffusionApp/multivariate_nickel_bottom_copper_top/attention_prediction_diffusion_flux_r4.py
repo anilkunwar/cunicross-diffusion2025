cb8f504b3e6afe401277daa3368dc32d0b0e4aa3
Anil Kunwar <anilkunwar318@yahoo.com>
2025-11-06 02:57:49 +0100

    Update attention_prediction_diffusion_flux_r4.py

--- pinn/diffusionApp/multivariate_nickel_bottom_copper_top/attention_prediction_diffusion_flux_r4.py
+++ pinn/diffusionApp/multivariate_nickel_bottom_copper_top/attention_prediction_diffusion_flux_r4.py
@@ -689,7 +689,7 @@ def plot_uphill_heatmap(solution, time_index, cmap='viridis', vmin=None, vmax=No
 
     if colorbar:
         #cbar = fig.colorbar(im2, ax=axes.ravel().tolist(), orientation='vertical',
-                            fraction=0.046, pad=0.08, shrink=0.8)
+        #                    fraction=0.046, pad=0.08, shrink=0.8)
         cbar = fig.colorbar(im2, ax=axes.ravel().tolist(), orientation='vertical',
                             fraction=0.046, pad=0.1, shrink=0.8)
         cbar.set_label(cbar_label, fontsize=label_fontsize, labelpad=10)
