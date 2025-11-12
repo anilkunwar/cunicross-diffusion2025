337ca675e867477b4918881ef5d0645581d11043
Anil Kunwar <anilkunwar318@yahoo.com>
2025-11-12 02:08:44 +0100

    Update attention_based_concentration_prediction_visualization_r4.py

--- pinn/diffusionApp/multivariate_nickel_bottom_copper_top/attention_based_concentration_prediction_visualization_r4.py
+++ pinn/diffusionApp/multivariate_nickel_bottom_copper_top/attention_based_concentration_prediction_visualization_r4.py
@@ -306,7 +306,8 @@ def plot_radar_single(data, element, t_val, fname, ly_spokes, show_labels=True, 
      # Control radial axis labels visibility
     if show_radial_labels:
         ax.set_yticks(ax.get_yticks())  # Show default radial ticks
-        ax.set_yticklabels([f"{int(tick):d}" if tick >= 0 else "" for tick in ax.get_yticks()], fontsize=12)
+        #ax.set_yticklabels([f"{int(tick):d}" if tick >= 0 else "" for tick in ax.get_yticks()], fontsize=12)
+        ax.set_yticklabels([f"{tick:.2f}" if tick >= 0 else "" for tick in ax.get_yticks()], fontsize=12)
     else:
         ax.set_yticklabels([])  # Hide radial axis labels
 
